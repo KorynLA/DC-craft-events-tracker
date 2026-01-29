9190cf8d89a2c5d14f2274e4ed4ac82e17a60362
KorynLA <koryn.lesliearcaya@gmail.com>
2026-01-29 15:04:49 -0500

    Update calendar with correct var & remove console logs

--- netlify/functions/calendar.js
+++ netlify/functions/calendar.js
@@ -1,11 +1,9 @@
 const axios = require("axios");
 
 exports.handler = async function (event, context) {
-  console.log(event);
-  console.log(context);
   try {
     const { id } = event.queryStringParameters;
-    const response = await axios.get(`${process.env.TODO_BASE_URL}/${id}`);
+    const response = await axios.get(`${process.env.CALENDAR_URL}/${id}`);
     return {
       statusCode: 200,
       body: JSON.stringify({ title: response.data.title }),
